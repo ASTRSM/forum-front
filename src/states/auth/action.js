8c125761d6f4fe89581515b7d75fc59008dd5ce8
astrsm <ddefrito84@gmail.com>
2023-01-17 14:18:52 +0700

    make test error

--- src/states/auth/action.js
+++ src/states/auth/action.js
@@ -30,7 +30,7 @@ function asyncSetAuthUser ({ email, password }) {
       api.putAccessToken(token)
       const authUser = await api.getOwnProfile()
 
-      dispatch(setAuthUserActionCreator(authUser))
+      dispatch(setAuthUserActionCreator())
     } catch (error) {
       alert(error.message)
     }
